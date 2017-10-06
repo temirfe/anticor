82436fbc3b7502fba363d5fcf5cff402105469ca
Temirbek Nuradil <temirbek@gmail.com>
2017-10-06 20:52:38 +0600

    inter'

--- app/src/main/java/kg/prosoft/anticorruption/NewsViewActivity.java
+++ app/src/main/java/kg/prosoft/anticorruption/NewsViewActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.AsyncTask;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
